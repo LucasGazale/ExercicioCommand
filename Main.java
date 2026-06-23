4bdb6dae7b04cbd39db4df3caecc84bc5ac092c9
LucasGazale <lucas.gazale@unifesp.br>
2026-06-23 16:19:50 -0300

    Add Pessoa class with id and nome attributes

---
+++ Main.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.function.Function;
+
+class Pessoa {
+    int id;
+    String nome;
+
+    Pessoa(int id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Pessoa{id=" + id + ", nome='" + nome + "'}";
+    }
+}
